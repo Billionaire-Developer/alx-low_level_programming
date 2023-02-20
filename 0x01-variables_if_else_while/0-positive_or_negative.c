07457306590a69ce8f2672f1f3219f85adacdc4f
Lawani Jeremiah <lawanijeremiah23@gmail.com>
2023-02-20 03:51:00 -0800

    positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,34 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
-
+/*more headers goes there */
 /**
- * main - print if numeber is positive, zero or nagative
- * Return: Always (success)
- * 
- */
+ * main -Entry point
+ *
+ * Description: generates random numbers, declares + OR -
+ *
+ *  Return: Always (0) Success
+*/
+
 int main(void)
 {
 	int n;
+
+
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 0)
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is nagative\n", n);
 	}
 	return (0);
 }
